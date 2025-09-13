7dc3dbcc1edfe4e3128bd81b1f07cb8c4958a63b
Masodi Success <109852676+suskidee@users.noreply.github.com>
2025-09-13 22:18:53 +0100

    setup custom user configurations

--- apps/user/admin.py
+++ apps/user/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import  User
+from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(DefaultUserAdmin):
+    list_display = ('email', 'is_staff', 'is_active', 'is_verified', 'phone_number')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'is_verified')
+    search_fields = ('email', 'phone_number')
+    ordering = ('email',)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Information', {'fields': ('phone_number',)}),
+        ('Permissions', {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'is_verified', 'groups', 'user_permissions')
+        }),
+        ('Important Dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active', 'is_verified')}
+         ),
+    )
--- apps/user/models.py
+++ apps/user/models.py
@@ -1,3 +1,68 @@
+import rest_framework_simplejwt.tokens
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from rest_framework_simplejwt.tokens import RefreshToken
+from apps.tenant.models import Tenant, Branch
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+
+    def _create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The email field must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        if password:
+            user.set_password(password)
+        else:
+            raise ValueError("Password must be provided")
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password=None, **extra_fields):
+
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if not extra_fields['is_staff']:
+            raise ValueError("Superuser must have is_staff=True.")
+        if not extra_fields['is_superuser']:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('admin', 'Admin'),
+        ('manager', 'Manager'),
+        ('employee', 'Employee'),
+    )
+    username = None
+    email = models.EmailField(unique=True)
+    is_verified = models.BooleanField(default=False)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    otp = models.IntegerField(null=True)
+    otp_created_at = models.DateTimeField(auto_now_add=True)
+    tenant = models.ForeignKey(Tenant, on_delete=models.CASCADE, null=True, blank=True)
+    branch = models.ManyToManyField(Branch)
+    role = models.CharField(choices=ROLE_CHOICES, default="admin")
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def tokens(self):
+        refresh = RefreshToken.for_user(self)
+        return ({
+            'refresh': str(refresh),
+            'access': str(refresh.access_token),
+        })
+
+    def __str__(self):
+        return self.email
