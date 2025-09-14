b93f1a5fa1547d264fb1cd534be40b9167299efd
Masodi Success <109852676+suskidee@users.noreply.github.com>
2025-09-14 13:20:13 +0100

    added utils to tenants

---
+++ apps/tenant/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+from .models import Tenant, Branch
+
+
+class TenantSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tenant
+        fields = ["id", "name", "industry", "created_at", "updated_at", "status"]
+        read_only_fields = ["id"]
+
+
+class SimpleTenantSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tenant
+        fields = ["id", "name", "industry", "created_at", "updated_at", "status"]
+
+
+class BranchSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Branch
+        fields = ["id", "name", "location", "created_at", "updated_at"]
+        read_only_fields = ["id"]
+
+
+class ViewBranchSerializer(serializers.ModelSerializer):
+    tenant = SimpleTenantSerializer()
+
+    class Meta:
+        model = Branch
+        fields = ["id", "tenant", "name", "location", "created_at", "updated_at"]
+        read_only_fields = ["id"]
+
+
--- apps/tenant/views.py
+++ apps/tenant/views.py
@@ -1,3 +1,62 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .serializers import TenantSerializer, BranchSerializer, ViewBranchSerializer
+from .models import Tenant, Branch
+from .utils import swagger_helper
 
-# Create your views here.
+
+class TenantView(ModelViewSet):
+    http_method_names = ["get", "post", "patch", "delete", "head", "options"]
+    serializer_class = TenantSerializer
+    queryset = Tenant.objects.all()
+
+    @swagger_helper("Tenant", "Tenant")
+    def list(self, *args, **kwargs):
+        return super().list(*args, **kwargs)
+
+    @swagger_helper("Tenant", "Tenant")
+    def retrieve(self, *args, **kwargs):
+        return super().retrieve(*args, **kwargs)
+
+    @swagger_helper("Tenant", "Tenant")
+    def create(self, *args, **kwargs):
+        return super().create(*args, **kwargs)
+
+    @swagger_helper("Tenant", "Tenant")
+    def partial_update(self, *args, **kwargs):
+        return super().partial_update(*args, **kwargs)
+
+    @swagger_helper("Tenant", "Tenant")
+    def destroy(self, *args, **kwargs):
+        return super().destroy(*args, **kwargs)
+
+
+class BranchView(ModelViewSet):
+    http_method_names = ["get", "post", "patch", "delete", "head", "options"]
+
+    def get_queryset(self):
+        Branch.objects.filter(user=self.request.user)
+
+    def get_serializer_class(self):
+        if self.request.method == "GET":
+            return ViewBranchSerializer
+        return BranchSerializer
+
+    @swagger_helper("Branch", "Branch")
+    def list(self, *args, **kwargs):
+        return super().list(*args, **kwargs)
+
+    @swagger_helper("Branch", "Branch")
+    def retrieve(self, *args, **kwargs):
+        return super().retrieve(*args, **kwargs)
+
+    @swagger_helper("Branch", "Branch")
+    def create(self, *args, **kwargs):
+        return super().create(*args, **kwargs)
+
+    @swagger_helper("Branch", "Branch")
+    def partial_update(self, *args, **kwargs):
+        return super().partial_update(*args, **kwargs)
+
+    @swagger_helper("Branch", "Branch")
+    def destroy(self, *args, **kwargs):
+        return super().destroy(*args, **kwargs)
