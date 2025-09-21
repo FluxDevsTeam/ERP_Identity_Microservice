b8fac60655fe2cff6be6d8dbf7a62f142b3bad29
Masodi Success <109852676+suskidee@users.noreply.github.com>
2025-09-21 12:55:50 +0100

    added user models

--- apps/user/models.py
+++ apps/user/models.py
@@ -3,6 +3,50 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from rest_framework_simplejwt.tokens import RefreshToken
 from apps.tenant.models import Tenant, Branch
+from django.conf import settings
+
+
+class NameChangeRequest(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="name_change_request")
+    new_first_name = models.CharField(max_length=150, null=True, blank=True)
+    new_last_name = models.CharField(max_length=150, null=True, blank=True)
+    new_phone_number = models.CharField(max_length=150, null=True, blank=True)
+    otp = models.IntegerField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Name Change Request for {self.user.email} - {self.new_first_name} {self.new_last_name}"
+
+
+class EmailChangeRequest(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="email_change_request")
+    new_email = models.EmailField(unique=True)
+    otp = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Email Change Request for {self.user.email} to {self.new_email}"
+
+
+class ForgotPasswordRequest(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    otp = models.IntegerField(null=True, blank=True)
+    new_password = models.CharField(max_length=128, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"ForgotPasswordRequest for {self.user.email}"
+
+
+class PasswordChangeRequest(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='password_change_requests')
+    otp = models.CharField(max_length=6)
+    new_password = models.CharField(max_length=128)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_verified = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Password change request for {self.user.username}"
 
 
 class UserManager(BaseUserManager):
@@ -28,17 +72,22 @@ class UserManager(BaseUserManager):
 
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_verified', True)
 
         if not extra_fields['is_staff']:
             raise ValueError("Superuser must have is_staff=True.")
         if not extra_fields['is_superuser']:
             raise ValueError("Superuser must have is_superuser=True.")
+        if not extra_fields['is_verified']:
+            raise ValueError("Superuser must have is_verified=True.")
 
         return self._create_user(email, password, **extra_fields)
 
 
 class User(AbstractUser):
     ROLE_CHOICES = (
+        ('ceo', 'CEO'),
+        ('Branch_manager', 'Branch_Manager'),
         ('admin', 'Admin'),
         ('manager', 'Manager'),
         ('employee', 'Employee'),
--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,4 +1,53 @@
-from django.urls import path, include
+from django.urls import path
+from .views import (
+    UserSignupViewSet,
+    UserLoginViewSet,
+    LogoutViewSet,
+    UserProfileViewSet,
+    ForgotPasswordViewSet,
+    PasswordChangeRequestViewSet,
+    GoogleAuthViewSet,
+    UserManagementViewSet
+)
 
 urlpatterns = [
+    # UserSignupViewSet
+    path('signup/', UserSignupViewSet.as_view({'post': 'create'}), name='user_signup'),
+    path('signup/verify-otp/', UserSignupViewSet.as_view({'post': 'verify'}), name='verify_otp'),
+    path('signup/resend-otp/', UserSignupViewSet.as_view({'post': 'resend_otp'}), name='resend_otp'),
+
+    # UserLoginViewSet
+    path('login/', UserLoginViewSet.as_view({'post': 'create'}), name='login'),
+    path('login/refresh-token/', UserLoginViewSet.as_view({'post': 'refresh_token'}), name='refresh_token'),
+
+    # LogoutViewSet
+    path('logout/', LogoutViewSet.as_view({'post': 'logout'}), name='logout'),
+
+    # UserProfileViewSet
+    path('profile/', UserProfileViewSet.as_view({'get': 'retrieve'}), name='user_profile'),
+    path('profile/request-email-change/', UserProfileViewSet.as_view({'post': 'request_email_change'}), name='request_email_change'),
+    path('profile/resend-email-change-otp/', UserProfileViewSet.as_view({'post': 'resend_email_change_otp'}), name='resend_email_change_otp'),
+    path('profile/verify-email-change/', UserProfileViewSet.as_view({'post': 'verify_email_change'}), name='verify_email_change'),
+    path('profile/request-profile-change/', UserProfileViewSet.as_view({'post': 'request_profile_change'}), name='request_profile_change'),
+    path('profile/verify-profile-change/', UserProfileViewSet.as_view({'post': 'verify_profile_change'}), name='verify_profile_change'),
+    path('profile/delete-account/', UserProfileViewSet.as_view({'post': 'delete_account'}), name='delete_account'),
+
+    # ForgotPasswordViewSet
+    path('forgot-password/request-forgot-password/', ForgotPasswordViewSet.as_view({'post': 'request_forgot_password'}), name='request_forgot_password'),
+    path('forgot-password/set-new-password/', ForgotPasswordViewSet.as_view({'post': 'set_new_password'}), name='set_new_password'),
+    path('forgot-password/verify-otp/', ForgotPasswordViewSet.as_view({'post': 'verify_otp'}), name='verify_otp'),
+    path('forgot-password/resend-otp/', ForgotPasswordViewSet.as_view({'post': 'resend_otp'}), name='resend_otp'),
+
+    # PasswordChangeRequestViewSet
+    path('password-change/request-password-change/', PasswordChangeRequestViewSet.as_view({'post': 'request_password_change'}), name='request_password_change'),
+    path('password-change/resend-otp/', PasswordChangeRequestViewSet.as_view({'post': 'resend_otp'}), name='resend_otp'),
+    path('password-change/verify-password-change/', PasswordChangeRequestViewSet.as_view({'post': 'verify_password_change'}), name='verify_password_change'),
+
+    # GoogleAuthViewSet
+    path('google-auth/google-auth/', GoogleAuthViewSet.as_view({'post': 'google_auth'}), name='google_auth'),
+    path('google-auth/set-google-auth-password/', GoogleAuthViewSet.as_view({'post': 'set_google_auth_password'}), name='set_google_auth'),
+
+    # UserManagementViewSet
+    path('users/', UserManagementViewSet.as_view({'get': 'list', 'post': 'create'}), name='user_management_list_create'),
+    path('users/<int:pk>/', UserManagementViewSet.as_view({'get': 'retrieve', 'patch': 'partial_update', 'delete': 'destroy'}), name='user_management_detail'),
 ]
